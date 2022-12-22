956990c4d1e201496e0f76ca3d10cb3228a853d7
mpprwork <119483323+mpprwork@users.noreply.github.com>
2022-12-22 22:43:10 +0200

    Initial commit
    when running  npx hardhat run scripts/deploy.js it deploys the store contract

---
+++ test/test.js
@@ -0,0 +1,6 @@
+const {time, loadFixture,} = require ("@nomicfoundation/hardhat-toolbox");
+
+const {anyValue} = require("@nomicfoundation/hardhat-chai-matchers/withArgs");
+
+const {expect} = require ("chai");
+const {ethers} = require("hardhat");
